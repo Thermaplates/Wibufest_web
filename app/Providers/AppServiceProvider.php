af542c2b18345acf34d07dff735cff0322a4f232
Marchel_INO <iwayansaputramarchel@gmail.com>
2025-09-08 07:56:50 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    
+
+public function boot()
+{
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 
     /**
      * Bootstrap any application services.
